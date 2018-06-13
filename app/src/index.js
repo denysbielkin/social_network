accccc8a7faeb9ee75e01f57ad9178ffb10c78ef
DenisBelkin <dan.98belkin@gmail.com>
2018-06-13 18:57:30 +0300

    day 2(form and my try in redux-form(in comments) )

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import {Provider} from 'react-redux'
 import App from './App';
+//import {createStore} from 'redux'
+import SignUpForm from './containers/SignUpForm'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+ReactDOM.render(
+
+        <div>
+            <App/>
+            <SignUpForm/>
+        </div>
+
+    , document.getElementById('root'));
 
